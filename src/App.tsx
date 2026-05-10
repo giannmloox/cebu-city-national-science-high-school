93ce5020579c0f7ef50cb54a8abd0a26a7c1ccf5
giannmloox <giannmloox@gmail.com>
2026-05-10 14:31:45 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Index from "./pages/Index.tsx";
 import NotFound from "./pages/NotFound.tsx";
 import ScihiZine from "./pages/ScihiZine.tsx";
 import Shop from "./pages/Shop.tsx";
+import Apply from "./pages/Apply.tsx";
 import BackToTop from "./components/BackToTop";
 
 const queryClient = new QueryClient();
@@ -21,6 +22,7 @@ const App = () => (
           <Route path="/" element={<Index />} />
           <Route path="/scihizine" element={<ScihiZine />} />
           <Route path="/shop" element={<Shop />} />
+          <Route path="/apply" element={<Apply />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
